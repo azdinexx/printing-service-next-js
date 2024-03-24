8cb83c82cab7bb9499d074927d32024e1c8b74be
azdinexx <azdinebouali2002@gmail.com>
2024-03-24 15:36:56 +0000

    Refactor page component to fetch uploads from environment variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,83 +15,87 @@ export type UploadType = {
 export const dynamic = 'force-dynamic';
 
 async function page() {
-  const uploads = await fetch(`http:localhost:3000/api/upload`).then((res) =>
-    res.json()
-  );
-  console.log(uploads);
-  return (
-    <div className='container p-4'>
-      <h1 className='text-xl my-4'>Uploaded Files</h1>
-      <div className='overflow-x-auto'>
-        <table className='table'>
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type='checkbox' className='checkbox' />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Owner</th>
-              <th>Size</th>
-              <th>Created At</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            {uploads.map((upload: UploadType) => (
-              <tr key={upload.Etag + upload.Etag}>
-                <td>
+  try {
+    const uploads = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/upload`
+    ).then((res) => res.json());
+    console.log(uploads);
+    return (
+      <div className='container p-4'>
+        <h1 className='text-xl my-4'>Uploaded Files</h1>
+        <div className='overflow-x-auto'>
+          <table className='table'>
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
                   <label>
                     <input type='checkbox' className='checkbox' />
                   </label>
-                </td>
-                <td>
-                  <Link
-                    href={`https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${upload.Key}`}
-                  >
-                    {upload.Key}
-                  </Link>
-                </td>
-                <td>{upload.Owner.DisplayName}</td>
-                <td>
-                  {Math.round(upload.Size / 1024) > 1024
-                    ? `${Math.round(upload.Size / 1024 / 1024)} MB`
-                    : `${Math.round(upload.Size / 1024)} KB`}
-                </td>
-                <td>
-                  <button className='btn glass btn-xs text-xs'>
-                    {Math.round(
-                      (new Date().getTime() -
-                        new Date(upload.LastModified).getTime()) /
-                        (1000 * 60 * 60 * 24)
-                    )}{' '}
-                    Days Ago
-                  </button>
-                </td>
-                <td>
-                  <button className='btn btn-danger'>Delete</button>
-                </td>
+                </th>
+                <th>Name</th>
+                <th>Owner</th>
+                <th>Size</th>
+                <th>Created At</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-          {/* foot */}
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Owner</th>
-              <th>Size</th>
-              <th>Created At</th>
-              <th>Action</th>
-            </tr>
-          </tfoot>
-        </table>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {uploads.map((upload: UploadType) => (
+                <tr key={upload.Etag + upload.Etag}>
+                  <td>
+                    <label>
+                      <input type='checkbox' className='checkbox' />
+                    </label>
+                  </td>
+                  <td>
+                    <Link
+                      href={`https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${upload.Key}`}
+                    >
+                      {upload.Key}
+                    </Link>
+                  </td>
+                  <td>{upload.Owner.DisplayName}</td>
+                  <td>
+                    {Math.round(upload.Size / 1024) > 1024
+                      ? `${Math.round(upload.Size / 1024 / 1024)} MB`
+                      : `${Math.round(upload.Size / 1024)} KB`}
+                  </td>
+                  <td>
+                    <button className='btn glass btn-xs text-xs'>
+                      {Math.round(
+                        (new Date().getTime() -
+                          new Date(upload.LastModified).getTime()) /
+                          (1000 * 60 * 60 * 24)
+                      )}{' '}
+                      Days Ago
+                    </button>
+                  </td>
+                  <td>
+                    <button className='btn btn-danger'>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            {/* foot */}
+            <tfoot>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Owner</th>
+                <th>Size</th>
+                <th>Created At</th>
+                <th>Action</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch {
+    return <div>something went wrong</div>;
+  }
 }
 
 export default page;
